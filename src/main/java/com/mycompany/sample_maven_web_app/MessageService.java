32daa5f2e81d769a672711d25437c7cd12d66aee
wackzingo <wackzingo@gmail.com>
2017-02-11 02:42:42 -0800

    src/scripts/create_message.sh 	src/scripts/message.json

--- src/main/java/com/mycompany/sample_maven_web_app/MessageService.java
+++ src/main/java/com/mycompany/sample_maven_web_app/MessageService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import objects.Messages;
@@ -26,6 +27,7 @@ import org.codehaus.jackson.map.ObjectMapper;
  *
  * @author ubuntu
  */
+@Path("messages")
 public class MessageService {
     
     public MessageService() {
