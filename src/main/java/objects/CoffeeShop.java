cfd5ca2c8a32ca363b08102a9e1817503493ffd3
wackzingo <wackzingo@gmail.com>
2017-02-16 02:55:39 -0800

    Fixing more bugs.

--- src/main/java/objects/CoffeeShop.java
+++ src/main/java/objects/CoffeeShop.java
@@ -9,7 +9,39 @@ package objects;
 
 public class CoffeeShop {
 
-	/* The name of the shop */
+    public String getMyshopname() {
+        return myshopname;
+    }
+
+    public void setMyshopname(String myshopname) {
+        this.myshopname = myshopname;
+    }
+
+    public String getMyaddress() {
+        return myaddress;
+    }
+
+    public void setMyaddress(String myaddress) {
+        this.myaddress = myaddress;
+    }
+
+    public String getMyphone() {
+        return myphone;
+    }
+
+    public void setMyphone(String myphone) {
+        this.myphone = myphone;
+    }
+
+    public int getMyshopId() {
+        return myshopId;
+    }
+
+    /* The name of the shop */
+    public void setMyshopId(int myshopId) {
+        this.myshopId = myshopId;
+    }
+
 	private String myshopname;
 	
 	/* The address of the shop */
@@ -17,40 +49,6 @@ public class CoffeeShop {
         
         private String myphone;
 	
-        private int myshopId;
-
-	
-        public int getShopId() {
-            return myshopId;
-        }
-
-        public void setShopId(int theShopId) {
-            this.myshopId = theShopId;
-        }
- 
-	
-	public String getShopName() {
-		return this.myshopname;
-	}
-        
-        public void setShopName(String theShopName) {
-            this.myshopname = theShopName;
-        }
-	
-	public String getAddress() {
-		return myaddress;
-	}
-        
-        public void setAddress(String theAddress) {
-            this.myaddress = theAddress;
-        }
-	
-        public String getPhone() {
-            return myphone;
-        }
-        
-        public void setPhone(String thePhone) {
-            this.myphone = thePhone;
-        }
+        private int myshopId;      
 
 }
