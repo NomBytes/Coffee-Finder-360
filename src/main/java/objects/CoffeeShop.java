f21aac26cc7ab803d3bf97bfbac9ee249c95c10c
Calvin Gunther <cdg57@uw.edu>
2017-03-01 19:11:14 -0800

    Added review scripts and JSON objects, changed some Shop JSON objs

--- src/main/java/objects/CoffeeShop.java
+++ src/main/java/objects/CoffeeShop.java
@@ -16,27 +16,35 @@ public class CoffeeShop {
     public void setMyshopname(String myshopname) {
         this.myshopname = myshopname;
     }
+
     public String getMyshopname(){
       return myshopname
     }
+
     public void setMyshopname(String name){
         this.myshopname = name;
     }
+
     public String getStreet(){
       return street;
     }
+
     public void setStreet(String street){
       this.myStreet = street;
     }
+
     public String getState(){
       return state;
     }
+
     public void setState( String state){
       this.myState = state;
     }
+
     public void setZip(int zip){
       this.myzip = zip;
     }
+
     public int getZip(){
       return myzip;
     }
@@ -60,22 +68,18 @@ public class CoffeeShop {
         return myshopId;
     }
 
-    /* The name of the shop */
     public void setMyshopId(int myshopId) {
         this.myshopId = myshopId;
     }
 
-	private String myshopname;
-
-	/* The address of the shop */
-//	private String myaddress;
-
-  private String myStreet;
-  private String mystate;
-  private int myzip;
-  private String myphone;
-  private int myshopId;
-  private String mylatitude;
-  private String mylongitude;
+    private String myshopname;
+    private String mycity;
+    private String mystreet;
+    private String mystate;
+    private int myzip;
+    private String myphone;
+    private int myshopId;
+    private String mylatitude;
+    private String mylongitude;
 
 }
