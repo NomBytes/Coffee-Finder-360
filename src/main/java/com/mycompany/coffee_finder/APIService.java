8d548e7228e3c2ffa16a06ca8fac548b5c889a2c
wackzingo <wackzingo@gmail.com>
2017-03-12 20:46:41 -0700

    Finished API Service.

--- src/main/java/com/mycompany/coffee_finder/APIService.java
+++ src/main/java/com/mycompany/coffee_finder/APIService.java
@@ -33,7 +33,7 @@ import org.codehaus.jackson.map.ObjectMapper;
  *
  * @author wlloyd
  */
-@Path("shops")
+@Path("api/shops")
 public class APIService {
 
     static final Logger logger = Logger.getLogger(CoffeeShopService.class.getName());
@@ -54,22 +54,123 @@ public class APIService {
     @GET
     @Path("{shopid}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<CoffeeShop> getShops(@PathParam("shopid") String shopid) {
-        LinkedList<CoffeeShop> lshops = new LinkedList<CoffeeShop>();
+    public List<CoffeeShops> getShops(@PathParam("shopid") String shopid) {
+        LinkedList<CoffeeShops> lshops = new LinkedList<CoffeeShops>();
         StringBuilder sb = new StringBuilder();
-        sb.append("[");
+        
         try
         {
             int sid = Integer.parseInt(shopid);
             ShopModel db = ShopModel.singleton();
             CoffeeShop[] shops = db.getShops(sid);
-            for (int i=0;i<shops.length;i++)
-                lshops.add(shops[i]);
+            for (int i=0;i<shops.length;i++) {
+                CoffeeShops c = new CoffeeShops(shops[i]);
+                lshops.add(c);
+            }
         }
         catch (Exception e)
         {
             sb.append("Error getting users: " + e.toString());
         }
         return lshops;
+    }
+    
+    
+    private class CoffeeShops {
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public long getZip() {
+            return zip;
+        }
+
+        public void setZip(long zip) {
+            this.zip = zip;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public int getOpen() {
+            return open;
+        }
+
+        public void setOpen(int open) {
+            this.open = open;
+        }
+
+        public int getClose() {
+            return close;
+        }
+
+        public void setClose(int close) {
+            this.close = close;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        
+        private String name;
+        private String address;
+        private String city;
+        private String state;
+        private long zip;
+        private String phone;
+        private int open;
+        private int close;
+        private String description;
+        
+        CoffeeShops(CoffeeShop c) {
+            name = c.getMyshopname();
+            address = c.getMystreet();
+            city = c.getMycity();
+            state = c.getMystate();
+            zip = c.getMyzip();
+            phone = c.getMyphone();
+            open = c.getMyopenhours();
+            close = c.getMyclosehours();
+            description = c.getMylatitude() +","+ c.getMylongitude();
+        }
     }
 }
